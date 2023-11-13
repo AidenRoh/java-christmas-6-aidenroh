2b6598acf05eb245771da4cb3b55b97e266e3c62
AidenRoh <colohrby@gmail.com>
2023-11-13 20:22:14 +0900

    feat: add convertor to process domain data for output

---
+++ src/main/java/christmas/util/Convertor.java
@@ -0,0 +1,35 @@
+package christmas.util;
+
+import static christmas.enums.Delimiter.GIFT_GOODS;
+
+import christmas.enums.Discount;
+import christmas.enums.Menu;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Convertor {
+	public static Map<String, Integer> menuItemEnumToMap(Map<Menu, Integer> detail) {
+		Map<String, Integer> giftMenu = new HashMap<>();
+
+		detail.forEach((key, value) -> giftMenu.put(key.getItem(), value));
+		return giftMenu;
+	}
+
+	public static Map<String, Integer> menuPriceEnumToMap(Map<Menu, Integer> detail) {
+		Map<String, Integer> benefitDetailsOfMenu = new HashMap<>();
+
+		detail.forEach((key, value) -> {
+			benefitDetailsOfMenu.put(GIFT_GOODS.getDelimiter(), key.getPrice());
+		});
+		return benefitDetailsOfMenu;
+	}
+
+	public static Map<String, Integer> discountEnumToMap(Map<Discount, Integer> detail) {
+		Map<String, Integer> benefitDetailsOfDiscount = new HashMap<>();
+
+		detail.forEach((key, value) -> {
+			benefitDetailsOfDiscount.put(key.getTitle(), value);
+		});
+		return benefitDetailsOfDiscount;
+	}
+}
