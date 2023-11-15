21f5279017dfb8e1e7fc41e689d46254efd6b09f
AidenRoh <colohrby@gmail.com>
2023-11-15 20:14:57 +0900

    refactor: add validation logic

--- src/main/java/christmas/domain/OrderDetail.java
+++ src/main/java/christmas/domain/OrderDetail.java
@@ -1,18 +1,26 @@
 package christmas.domain;
 
-import christmas.enums.Menu;
+import static christmas.exception.InvalidMenuErrorMessage.BEVERAGE_ONLY_ORDER;
+import static christmas.exception.InvalidMenuErrorMessage.MAXIMUM_AMOUNT_OF_ORDER;
+import static christmas.validation.DomainValidator.validateBeverageOnly;
+import static christmas.validation.DomainValidator.validateMaximumOrderAmount;
+
+import christmas.domain.constant.Menu;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 
 public class OrderDetail {
+
 	private final Map<Menu, Integer> orderDetail;
 
 	private OrderDetail(Map<Menu, Integer> orderDetail) {
 		this.orderDetail = orderDetail;
 	}
 
-	public static OrderDetail of(HashMap<String, Integer> orderMenu) {
+	public static OrderDetail of(Map<String, Integer> orderMenu) {
+		BEVERAGE_ONLY_ORDER.findErrorBy(() -> validateBeverageOnly(orderMenu));
+		MAXIMUM_AMOUNT_OF_ORDER.findErrorBy(() -> validateMaximumOrderAmount(orderMenu));
+
 		Map<Menu, Integer> confirmedOrder = new EnumMap<>(Menu.class);
 		orderMenu.forEach((key, value) -> confirmedOrder.put(Menu.findMenu(key), value));
 		return new OrderDetail(confirmedOrder);
