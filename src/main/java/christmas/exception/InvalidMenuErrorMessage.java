21b6479d05d77f503b4cb1936984354d16c8bfc2
AidenRoh <colohrby@gmail.com>
2023-11-15 22:08:31 +0900

    chore: text revision

--- src/main/java/christmas/exception/InvalidMenuErrorMessage.java
+++ src/main/java/christmas/exception/InvalidMenuErrorMessage.java
@@ -13,7 +13,7 @@ public enum InvalidMenuErrorMessage {
 	INVALID_MENU_AMOUNT("메뉴 수량은 1 이자상 20 이하여야 합니다."),
 	BLANK_EXIST("두 쉼표(,) 사이에 빈 값이 존재합니다."),
 	BAD_DELIMITER_COMMA("메뉴는 쉼표(,)로 시작하거나 끝낼 수 없습니다."),
-	BAD_DELIMITER_HYPHEN("메뉴는 (-)로 시작하거나 끝낼 수 없습니다."),
+	BAD_DELIMITER_HYPHEN("메뉴는 하이픈(-)으로 시작하거나 끝낼 수 없습니다."),
 	INVALID_FORMAT("메뉴 형식(메뉴-수량)에 맞지 않습니다.");
 
 	private final String message;
