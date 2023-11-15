9a3ea240549b70afae0b17985002baeadf6c2f33
AidenRoh <colohrby@gmail.com>
2023-11-15 20:16:58 +0900

    feat: initialization logic in main

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,30 @@
 package christmas;
 
+import christmas.controller.PromotionController;
+import christmas.domain.DiscountCenter;
+import christmas.domain.constant.Menu;
+import christmas.domain.discount.ChristmasDiscount;
+import christmas.domain.discount.SpecialDiscount;
+import christmas.domain.discount.WeekdayDiscount;
+import christmas.domain.discount.WeekendDiscount;
+import christmas.ui.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+
+		DiscountCenter discountCentre = new DiscountCenter(
+				new ChristmasDiscount(),
+				new SpecialDiscount(),
+				new WeekendDiscount(),
+				new WeekdayDiscount()
+		);
+		OutputView outputView = new OutputView();
+
+		PromotionController controller = new PromotionController(
+				discountCentre,
+				Menu.CHAMPAGNE,
+				outputView);
+
+		controller.makeReservation();
+	}
 }
