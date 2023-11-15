934037091709a85fc58d29a6591c69fbc434ade1
AidenRoh <colohrby@gmail.com>
2023-11-15 20:39:57 +0900

    refactor: fix find valid menu test logic

--- src/test/java/christmas/enums/MenuTest.java
+++ src/test/java/christmas/enums/MenuTest.java
@@ -1,7 +1,10 @@
 package christmas.enums;
 
+import static christmas.exception.InvalidMenuErrorMessage.NON_EXIST_MENU;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import christmas.exception.ChristmasPromotionException;
+import christmas.validation.UIValidator;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -12,9 +15,9 @@ class MenuTest {
 	void 메뉴에_없는_음식을_주문할_시_예외가_발생한다() {
 		//given
 		String nonMenuValue = "감자수프";
-		//when
-		assertThatThrownBy(() -> Menu.findMenu(nonMenuValue))
-				//then
-				.isInstanceOf(IllegalArgumentException.class);
+		//when && then
+		assertThatThrownBy(() -> {
+			NON_EXIST_MENU.findErrorBy(() -> UIValidator.validateExistMenu(nonMenuValue));
+		}).isInstanceOf(ChristmasPromotionException.class);
 	}
 }
